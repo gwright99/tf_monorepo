7dec96aa3dd132d1bf5fb8a68763944cf92b2144
Graham Wright <graham.wright@seqera.io>
2024-09-22 11:07:20 -0400

    Tinkering with packing logic

--- infrastructure/module_deployment/main.py
+++ infrastructure/module_deployment/main.py
@@ -26,9 +26,24 @@ if __name__ == "__main__":
     for module, version in config.items():
         print(f"Beginning to zip module {module}-{version}.")
 
-        zip_output_name = (
-            f"{os.getcwd()}/infrastructure/modules/{module}/{module}-{version}"
-        )
+        # zip_output_name = (
+        #     f"{os.getcwd()}/infrastructure/modules/{module}/{module}-{version}"
+        # )
+        # directory_name = f"{os.getcwd()}/infrastructure/modules/{module}/"
+
+        # shutil.make_archive(zip_output_name, format="zip", root_dir=directory_name)
+        # print(f"Done zipping module {module}-{version}.")
+
+        # print(f"Uploading {module}-{version} to S3.")
+        # s3_client.upload_file(
+        #     f"{zip_output_name}.zip", BUCKET_NAME, f"{module}/{version}.zip"
+        # )
+        # print(f"Done uploading {module}-{version} to S3.")
+
+        # Packaged modules contain another (empty) copy of the archive (e.g. main.tf and vpc-1.0.0.zip)
+        # I think this is due to zip_output_name pointing to the same directory. Changing to /tmp
+
+        zip_output_name = f"/tmp/{module}-{version}"
         directory_name = f"{os.getcwd()}/infrastructure/modules/{module}/"
 
         shutil.make_archive(zip_output_name, format="zip", root_dir=directory_name)
